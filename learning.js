e5ed828c2d41c66de4bc90eee4473d75d0125814
HarshPatel <harshpatel362460@gmail.com>
2024-12-17 12:44:36 +0530

    hi

---
+++ learning.js
@@ -0,0 +1,57 @@
+/*
+   javascript is used to intractive youe web page.
+
+   variable
+console.log
+document.write()
+typeof
++ operator 
+if-elseif-else
+confirm
+for
+array
+function 
+class 
+object
+multi dimensional array and object
+forEach
+
+
+How we can display data in js file.
+
+  console.log() = print data in console tab.
+  document.write() = it is display data on browser.
+
+
+  variable= variable is a one type of storage data.
+
+  How to create variable in java script?
+   in java script 3 way to create a variable.
+     var variablename = value; -> we can create normal variable using var key word so we can change value using redeclaration process
+                        and using some assignment oparetor
+     let variablename = value;-> we can change value using any assigment oparetor or increament decreament oparetor but we can't redecler variaable.
+  
+  
+     const variablename = value; -> fix value can not change
+*/
+
+console.log("Hello Good Morning");
+document.write('Today We are start to learn about java script');
+document.getElementById('p-5').innerHTML = 'Harsh'
+var Company_name = 'ThatsEnd';
+console.log(Company_name);
+var Company_type = 'pvt ltd';
+console.log(Company_type);
+var Company_name = 'ThatsEnd technology';
+console.log(Company_name);
+let Company_gst_number = 1645372828;
+console.log(Company_gst_number);
+//let Company_gst_number = 1631245372828;
+Company_gst_number++;
+console.log(Company_gst_number);
+const Company_CEO = 'NiravRaj';
+console.log(Company_CEO);
+//const Company_CEO = 'Dileshsinh';
+const num = 120;
+//num++;
+console.log(num);
