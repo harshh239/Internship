9bbc6773d8147bd6a509ed15ac7081e5b46816af
HarshPatel <harshpatel362460@gmail.com>
2024-12-19 13:20:14 +0530

    h

--- learning.js
+++ learning.js
@@ -174,3 +174,7 @@ console.log(a == b);
 console.log(a != b);
 console.log(a >= b);
 console.log(a <= b);
+console.log(a == 340);
+console.log(a === 340);
+console.log(a == '340');
+console.log(a === '340');
